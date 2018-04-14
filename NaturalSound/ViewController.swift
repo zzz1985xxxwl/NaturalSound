50b70b9ba247b3c724e24971acd9dad10f549ce4
adrain.xue <adrain.xue@transfinder.com>
2018-04-15 07:36:38 +0800

    Initial Commit

---
+++ NaturalSound/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NaturalSound
+//
+//  Created by 薛文龙 on 2018/4/15.
+//  Copyright © 2018年 com.adrainxue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
